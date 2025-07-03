e9c2679d158d4859d08b5581c772714d246b066e
Vitaly Kichenko <vkichenko@gmail.com>
2025-07-04 01:03:32 +0300

    decor bot

--- bot.py
+++ bot.py
@@ -1,37 +1,55 @@
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Give me name and phone please."
+        except IndexError:
+            return "Invalid input. Format: phone <name>."
+        except KeyError:
+            return "Contact not found."
+        except Exception as e:
+            return f"An unexpected error occurred: {e}"
+    return inner
+
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, *args
 
+@input_error
 def add_contact(args, contacts):
-    try:
+    # try:
         name, phone = args
         contacts[name] = phone
         return "Contact added."
-    except ValueError:
-        return "Invalid input. Format: add <name> <phone_number>."
+    # except ValueError:
+    #     return "Invalid input. Format: add <name> <phone_number>."
 
+@input_error
 def change_contact(args, contacts):
-    try:
+    # try:
         name, phone = args
         if name in contacts:
             contacts[name] = phone
             return "Contact updated."
         else:
             return "Contact not found."
-    except ValueError:
-        return "Invalid input. Format: change <name> <new_phone_number>."
+    # except ValueError:
+    #     return "Invalid input. Format: change <name> <new_phone_number>."
 
+@input_error
 def show_phone(args, contacts):
-    try:
+    # try:
         name = args[0]
         if name in contacts:
             return f"{name}'s phone number is {contacts[name]}."
         else:
             return "Contact not found."
-    except IndexError:
-        return "Invalid input. Format: phone <name>."
+    # except IndexError:
+    #     return "Invalid input. Format: phone <name>."
 
+@input_error
 def show_all(contacts):
     list = "All contacts:\n"
     if contacts:
@@ -40,6 +58,9 @@ def show_all(contacts):
         return list
     else:
         return "No contacts available."
+
+
+
 
 
 def main():
